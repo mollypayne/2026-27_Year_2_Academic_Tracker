535fafec272823022ff549bbd95579a8173ac7ec
Molly <not.so.mxlly@gmail.com>
2026-09-13 15:22:58 +0100

    Update activity names in activities.sql

--- activities.sql
+++ activities.sql
@@ -7,5 +7,4 @@ CREATE TABLE activities (
 
 INSERT INTO activities (activity_id, activity_name, activity_category, role)
 VALUES
-(1, 'Women in STEM Freshers Fair', 'Society', 'Vice President'),
-(2, 'Women in STEM Welcome Event', 'Society', 'Vice President');
+(1, 'Women in STEM', 'Society', 'Vice President');
